c0bd76ddc2b91a88b20253d90d4660745dc85db0
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-01-02 15:36:11 +0100

    added functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-console.log("Cześć")
-
-let button = document.querySelector(".button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
-let table = document.querySelector(".table");
-button.addEventListener("click", () => {
-    table.classList.toggle("table--display");
-    if (table.classList.contains("table--display")) {
-        themeName.innerText = "Pokaż"
-    } else {
-        themeName.innerText = "Ukryj"
+{
+    const welcome= () => {
+        console.log("Cześć");
     }
-})
+    
+    const showTableClick = () => {
+        const themeName = document.querySelector(".themeName");
+        const table = document.querySelector(".table");
+
+        table.classList.toggle("table--display");
+        if (table.classList.contains("table--display")) {
+            themeName.innerText = "Pokaż"
+        } else {
+            themeName.innerText = "Ukryj"
+        };
+       
+    };
+    
+    const init = () => {
+        const button = document.querySelector(".button"); 
+   
+        button.addEventListener("click", showTableClick);
+
+        welcome();
+    };
+
+    init();
+}
